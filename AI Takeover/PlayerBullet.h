8aff818ac97b422418190dae4109f802ce3e64c0
Julian <jdhanda22@kentdenver.org>
2019-07-19 13:41:56 -0400

    bulletplayer

--- AI Takeover/PlayerBullet.h
+++ AI Takeover/PlayerBullet.h
@@ -1,9 +1,27 @@
 #pragma once
+#include "pch.h"
+#include "Pounder.h"
+#include "Ranger.h"
+#include "RangerBullet.h"
+#include "PlayerBullet.h"
+#include <iostream>
+#include <string>
+#include <random>
+#include <fstream>
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+
+using namespace std;
+using namespace sf;
+
 class PlayerBullet
 {
 public:
-	PlayerBullet();
-	// Texture bulletskin;
-	// Sprite bullet;
+	PlayerBullet(Vector2f);
+	Texture bulletskin;
+	Sprite bullet;
+	Clock bulletmover;
+	void MoveBullet(int);
 };
 
