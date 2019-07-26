46abda83b24807c0fefed06ef3b7713914a66849
Julian <jdhanda22@kentdenver.org>
2019-07-26 11:32:19 -0400

    rtgerfwd

--- AI Takeover/Boss.h
+++ AI Takeover/Boss.h
@@ -1,4 +1,18 @@
 #pragma once
+#include "pch.h"
+#include "Ranger.h"
+#include "RangerBullet.h"
+#include "PlayerBullet.h"
+#include <iostream>
+#include <string>
+#include <random>
+#include <fstream>
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+using namespace std;
+using namespace sf;
+
 class Boss
 {
 public:
